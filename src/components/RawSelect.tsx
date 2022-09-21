4e9386572d724d562e3b05929a7b1c2e75945cc8
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-09-20 22:01:09 -0600

    feat: add divider and caret

--- src/components/RawSelect.tsx
+++ src/components/RawSelect.tsx
@@ -18,6 +18,8 @@ export const Select: FC<SelectProps> = ({ value, options, onChange }) => {
     <div className={styles.container}>
       <span className={styles.value}>Value</span>
       <button className={styles["clear-btn"]}>&times;</button>
+      <div className={styles.divider}></div>
+      <div className={styles.caret}></div>
     </div>
   );
 };
